8a95c3ad3358b5a720245434598abde07f005dcb
Yoonkeee <yoonkeee@gmail.com>
2024-01-29 03:15:54 +0900

    style: 인증화면 레이아웃 수정

--- frontend/src/routes/Authentication.jsx
+++ frontend/src/routes/Authentication.jsx
@@ -1,5 +1,5 @@
 import { useRecoilState, useSetRecoilState } from 'recoil';
-import { Flex, HStack, Image, Text, VStack } from '@chakra-ui/react';
+import { Box, Flex, HStack, Image, Text, VStack } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import {
   authenticationAtom,
@@ -133,9 +133,19 @@ export const Authentication = () => {
       pt={'5vh'}
       w="100%"
     >
-      <HStack h={'16%'} gap={1} w={'100%'} justifyContent={'center'}>
-        <Image filter="invert(1)" src={LogoDog} h={'80%'} />
-        <Image filter="invert(1)" src={LogoWord} h={'50%'} />
+      <HStack h={'16%'} gap={1} w={'100%'} justifyContent={'center'} minH={'16%'}>
+        <Image
+          filter="invert(1)"
+          src={LogoDog}
+          h={'80%'}
+          fallback={<Box h={'80%'} aspectRatio={1.24} bgColor={'transparent'} />}
+        />
+        <Image
+          filter="invert(1)"
+          src={LogoWord}
+          h={'50%'}
+          fallback={<Box h={'50%'} aspectRatio={3.1} bgColor={'transparent'} />}
+        />
       </HStack>
       {failure.status ? (
         <Locked remainingTime={remainingTime} />
