264d310b8a24a2fbe95258b96bba6d711fac4597
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-07-14 23:16:57 +0900

    #8 feat: 상품 목록 조회 api 작성

--- frontend/src/mockApi.tsx
+++ frontend/src/mockApi.tsx
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import type { ProductWithSalesRecord } from 'src/types/dto';
 
 const mockBaseUrl = '/mocks';
 const mockInstance = axios.create({
   baseURL: mockBaseUrl,
 });
+
+export const getProductList = async () =>
+  mockInstance.get<ProductWithSalesRecord[]>('/get/product-list').then(response => response.data);
