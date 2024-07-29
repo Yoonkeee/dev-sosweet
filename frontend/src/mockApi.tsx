5429ba0a41ea57e60349eb9b8e0f7f6fa9599683
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-07-29 23:30:48 +0900

    #12 feat: 상품 추가 MockAPI 작성

--- frontend/src/mockApi.tsx
+++ frontend/src/mockApi.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import type { ProductWithSalesRecord } from 'src/types/dto';
+import type { ProductWithoutSalesRecord, ProductWithSalesRecord } from 'src/types/dto';
 
 const mockBaseUrl = '/mocks';
 const mockInstance = axios.create({
@@ -8,3 +8,6 @@ const mockInstance = axios.create({
 
 export const getProductList = async () =>
   mockInstance.get<ProductWithSalesRecord[]>('/get/product-list').then(response => response.data);
+
+export const addNewProduct = (newProduct: Omit<ProductWithoutSalesRecord, 'id' | 'itemsReceivedCount'>) =>
+  mockInstance.post('/post/add-new-product', newProduct);
